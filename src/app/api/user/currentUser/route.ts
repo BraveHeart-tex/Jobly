c753587d97c60f1979369d75160447e32ea8a8de
Bora <bora@Bora-MacBook-Air.local>
2023-06-11 07:49:53 +0300

    implemented get current user endpoint

---
+++ src/app/api/user/currentUser/route.ts
@@ -0,0 +1,27 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import { NextResponse } from 'next/server';
+
+// GET Current User
+export async function GET(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.json(
+      {
+        error: 'Not authenticated',
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  return NextResponse.json(
+    {
+      currentUser,
+    },
+    {
+      status: 200,
+    }
+  );
+}
