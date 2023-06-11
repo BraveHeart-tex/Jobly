b9104bff776514600e284a434fb0d1b0e3a72d2c
Bora <bora@Bora-MacBook-Air.local>
2023-06-11 15:03:59 +0300

    implemented paging, sorting and filtering searching functionality for salary estimation dataset page

--- src/app/api/salaryData/route.ts
+++ src/app/api/salaryData/route.ts
@@ -1,15 +1,35 @@
 import prisma from '@/app/libs/prismadb';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-  const pageNumber = 2;
-  const pageSize = 10;
+export async function GET(request: NextRequest) {
+  const pageParam = request.nextUrl.searchParams.get('page') || '1';
+  const sortParam = request.nextUrl.searchParams.get('sort') || 'desc';
+
+  const searchParam = request.nextUrl.searchParams.get('search') || '';
+
+  const pageSize = 20;
+  const pageNumber = parseInt(pageParam);
 
   const skipAmount = (pageNumber - 1) * pageSize;
 
-  const salaryData = await prisma.salaryEstimateDataset.findMany({
+  const salaryData = await prisma.salaryEstimationDataset.findMany({
     skip: skipAmount,
     take: pageSize,
+    where: {
+      jobTitle: {
+        contains: searchParam,
+      },
+    },
+    orderBy: {
+      salary_estimate: sortParam as 'asc' | 'desc',
+    },
+
+    select: {
+      id: true,
+      jobTitle: true,
+      location: true,
+      salary_estimate: true,
+    },
   });
 
   return NextResponse.json(salaryData);
