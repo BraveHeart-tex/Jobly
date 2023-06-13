90b1ac025b0d205feb8b71139787ec0ab80768d2
Bora <bora@Bora-MacBook-Air.local>
2023-06-13 10:58:19 +0300

    change base url

--- src/app/utils/customFetch.ts
+++ src/app/utils/customFetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://app-jobly.vercel.app/api',
 });
 
 export default customFetch;
