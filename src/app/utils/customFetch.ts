c6eeb1df30bbb71e16dfef53a7cf53286c989f53
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2023-11-11 23:08:58 +0300

    delete unused files

--- src/app/utils/customFetch.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const customFetch = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? "https://app-jobly.vercel.app/api" : "http://localhost:3000/api",
-});
-
-export default customFetch;
