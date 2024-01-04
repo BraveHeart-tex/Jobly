d73deb1ea74367f120354654a537afb9459d25b7
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-01-04 13:47:40 +0300

    small animation fix on responsive chart container

--- src/app/components/ResponsiveChartContainer.tsx
+++ src/app/components/ResponsiveChartContainer.tsx
@@ -1,4 +1,5 @@
 import { getMonthlyChartData } from "../actions";
+import MotionDiv from "../animations/MotionDiv";
 import BarChartComponent from "./Charts/BarChartComponent";
 import NoJobsFound from "@/components/NoJobsFound";
 
@@ -14,13 +15,23 @@ const ResponsiveChartContainer = async () => {
   }
 
   return (
-    <div className="flex justify-center items-start flex-col gap-2">
-      <h2 className="font-semibold text-foreground text-4xl text-facebook capitalize">Your Applications by Month</h2>
-      <p className="text-foreground">Below chart shows the number of applications you have submitted each month.</p>
+    <MotionDiv
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.3, type: "tween" }}
+      className="flex justify-center items-start flex-col gap-2"
+    >
+      <h2 className="font-semibold text-4xl text-facebook-500 dark:text-foreground capitalize">
+        Your Applications by Month
+      </h2>
+      <p className="text-gray-500 dark:text-gray-400 text-md">
+        Below chart shows the number of applications you have submitted each month.
+      </p>
       <div className="w-full h-[400px] flex justify-center items-center">
         <BarChartComponent data={result.monthlyApplicationsData} />
       </div>
-    </div>
+    </MotionDiv>
   );
 };
 
