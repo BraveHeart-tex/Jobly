f7e8e870b1e93488c73f51cfe59cafa7f7d681e0
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-01-04 14:16:12 +0300

    animation on auth pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,11 +2,18 @@ import { SignIn } from "@clerk/nextjs";
 import Image from "next/image";
 import AppLogo from "@/app/assets/logo.svg";
 import "@/app/cardStyles.css";
+import MotionDiv from "@/app/animations/MotionDiv";
 
 export default function Page() {
   return (
     <div className="flex min-h-screen lg:items-center justify-center bg-card dark:bg-gray-900">
-      <div className="bg-facebook dark:bg-gray-800 lg:rounded-md lg:shadow-lg max-w-[1200px] grid grid-cols-1 lg:grid-cols-2 w-full">
+      <MotionDiv
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, type: "tween" }}
+        className="bg-facebook dark:bg-gray-800 lg:rounded-md lg:shadow-lg max-w-[1200px] grid grid-cols-1 lg:grid-cols-2 w-full"
+      >
         <div className="flex flex-col lg:items-start p-4 lg:p-8 items-center h-max lg:h-auto">
           <Image src={AppLogo} alt="Jobly Logo" width={200} className="mb-2 dark:invert invert-[1]" />
           <div className="flex flex-col gap-1 lg:mt-auto">
@@ -17,7 +24,7 @@ export default function Page() {
         <div className="dark:bg-gray-900 bg-card flex items-center justify-center">
           <SignIn />
         </div>
-      </div>
+      </MotionDiv>
     </div>
   );
 }
