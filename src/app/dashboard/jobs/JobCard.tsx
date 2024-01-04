9f3f651cdf9e6faac46e038e8dc88b2dbf9f3070
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-01-04 18:52:19 +0300

    small fix on JobCard.tsx

--- src/app/dashboard/jobs/JobCard.tsx
+++ src/app/dashboard/jobs/JobCard.tsx
@@ -17,8 +17,9 @@ const JobCard = ({ jobApplication }: IJobCardProps) => {
     <MotionDiv
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
+      // @ts-ignore
       transition={{ duration: 0.5, type: "just" }}
-      exit={{ opacity: 0, y: 20, transition: { duration: 5, type: "just" } }}
+      exit={{ opacity: 0, y: 20 }}
       key={jobApplication.id}
       layoutId={`job-card-${jobApplication.id}`}
       className="relative p-6 bg-card dark:bg-gray-800 shadow-md rounded-md flex justify-between flex-col"
