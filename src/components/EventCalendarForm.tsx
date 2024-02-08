19648b7c89a2b387e49212fe5ea4e81f67f486a6
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-02-08 09:47:48 +0300

    Update import paths in EventCalendarForm component

--- src/components/EventCalendarForm.tsx
+++ src/components/EventCalendarForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface IEventCalendarFormProps {
   handleFormSubmit: (e: React.FormEvent<HTMLFormElement>, title: string) => void;
