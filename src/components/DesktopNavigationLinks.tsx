6298a1e3516e40e3a8d3ce257944670d6a995859
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-06-08 15:54:18 +0300

    chore: refactor desktop navigation links to its own component

---
+++ src/components/DesktopNavigationLinks.tsx
@@ -0,0 +1,75 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { EMPLOYEE_NAVIGATION_LINKS } from "@/lib/constants";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+const DesktopNavigationLinks = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {EMPLOYEE_NAVIGATION_LINKS.map((menuItem) => (
+          <NavigationMenuItem key={menuItem.triggerLabel}>
+            <NavigationMenuTrigger>{menuItem.triggerLabel}</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-2">
+                {menuItem.linkItems.map((linkItem) => (
+                  <NavigationLinkItem href={linkItem.href} title={linkItem.title} key={linkItem.title}>
+                    {linkItem.description}
+                  </NavigationLinkItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        ))}
+
+        <NavigationMenuItem>
+          <Link href="/search" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={cn(
+                navigationMenuTriggerStyle(),
+                "bg-primary hover:bg-primary/90 text-primary-foreground hover:text-primary-foreground",
+              )}
+            >
+              Find Work
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+export default DesktopNavigationLinks;
+
+const NavigationLinkItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWithoutRef<"a">>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className,
+            )}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-3 text-sm leading-snug text-muted-foreground">{children}</p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  },
+);
+
+NavigationLinkItem.displayName = "NavigationLinkItem";
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,79 +1,8 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
-import * as React from "react";
-import { cn } from "@/lib/utils";
 import MobileNavigationLinks from "@/components/MobileNavigationLinks";
-import { EMPLOYEE_NAVIGATION_LINKS } from "@/lib/constants";
-
-const NavigationLinks = () => {
-  return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        {EMPLOYEE_NAVIGATION_LINKS.map((menuItem) => (
-          <NavigationMenuItem key={menuItem.triggerLabel}>
-            <NavigationMenuTrigger>{menuItem.triggerLabel}</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-2">
-                {menuItem.linkItems.map((linkItem) => (
-                  <NavigationLinkItem href={linkItem.href} title={linkItem.title} key={linkItem.title}>
-                    {linkItem.description}
-                  </NavigationLinkItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        ))}
-
-        <NavigationMenuItem>
-          <Link href="/search" legacyBehavior passHref>
-            <NavigationMenuLink
-              className={cn(
-                navigationMenuTriggerStyle(),
-                "bg-primary hover:bg-primary/90 text-primary-foreground hover:text-primary-foreground",
-              )}
-            >
-              Find Work
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  );
-};
-
-const NavigationLinkItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWithoutRef<"a">>(
-  ({ className, title, children, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className,
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-3 text-sm leading-snug text-muted-foreground">{children}</p>
-          </a>
-        </NavigationMenuLink>
-      </li>
-    );
-  },
-);
-
-NavigationLinkItem.displayName = "NavigationLinkItem";
+import DesktopNavigationLinks from "@/components/DesktopNavigationLinks";
 
 const Navbar = () => {
   return (
@@ -89,7 +18,7 @@ const Navbar = () => {
         </Link>
 
         <div className="hidden lg:block">
-          <NavigationLinks />
+          <DesktopNavigationLinks />
         </div>
       </div>
     </nav>
