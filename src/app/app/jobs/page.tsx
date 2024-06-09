7e98fd7994e78efd3d0c6919ea124ef6e3a73ce5
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-06-09 17:50:21 +0300

    style: add biome config, remove prettier and eslint

--- src/app/app/jobs/page.tsx
+++ src/app/app/jobs/page.tsx
@@ -6,21 +6,21 @@ import { ROUTES } from "@/lib/constants";
 import JobsList from "./_components/JobsList";
 
 const JobsPage = async () => {
-  const { user } = await validateRequest();
+	const { user } = await validateRequest();
 
-  if (!user) {
-    redirect(ROUTES.LOGIN);
-  }
+	if (!user) {
+		redirect(ROUTES.LOGIN);
+	}
 
-  const jobs = await api.job.getJobListings();
+	const jobs = await api.job.getJobListings();
 
-  return (
-    <div className="bg-muted p-2 pt-0">
-        <div className="mx-auto max-w-screen-2xl">
-          <JobsList jobs={jobs} />
-        </div>
-      </div>
-  );
+	return (
+		<div className="bg-muted p-2 pt-0">
+			<div className="mx-auto max-w-screen-2xl">
+				<JobsList jobs={jobs} />
+			</div>
+		</div>
+	);
 };
 
 export default JobsPage;
