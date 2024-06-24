78d6030bcddcff1fe97fe37cf85497924310f7e3
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-06-24 10:24:10 +0300

    feat: add mock ui for cv preview

--- src/app/app/tools/cv-builder/create/page.tsx
+++ src/app/app/tools/cv-builder/create/page.tsx
@@ -3,6 +3,8 @@ import { useRef } from "react";
 import DocumentBuilderHeader from "../../_components/DocumentBuilderHeader";
 import DocumentBuilderViewToggler from "../../_components/DocumentBuilderViewToggler";
 import CvBuilderPersonalDetailsSection from "../../_components/CvBuilderPersonalDetailsSection";
+import { Button } from "@/components/ui/button";
+import { Check, Cloud } from "lucide-react";
 
 const CreateCvPage = () => {
   const builderContainerRef = useRef<HTMLDivElement | null>(null);
@@ -20,8 +22,27 @@ const CreateCvPage = () => {
           <CvBuilderPersonalDetailsSection />
         </div>
       </div>
-      <div className="bg-muted-foreground min-h-screen flex items-center justify-center flex-col">
-        Resume Live Preview
+      <div className="bg-muted-foreground dark:bg-secondary min-h-screen">
+        <div className="h-[91vh] w-[63%] mx-auto pt-4">
+          <div className="w-full flex items-center justify-end mb-4">
+            <Button className="self-end">Download PDF</Button>
+          </div>
+          <div className="bg-background rounded-md h-full w-full" />
+          <div className="flex items-center justify-between mt-2">
+            <div className="flex items-center gap-1">
+              <div className="relative">
+                <Cloud size={28} strokeWidth={1} />
+                <Check
+                  className="absolute top-[11px] left-[8px]"
+                  size={10}
+                  strokeWidth={2}
+                />
+              </div>
+              <span className="text-xs text-muted-foreground">Saved</span>
+            </div>
+            <div />
+          </div>
+        </div>
       </div>
     </main>
   );
