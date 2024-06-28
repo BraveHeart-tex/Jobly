5ae3b00cd86eff78ca83056501399d0c06438d7c
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-06-28 23:11:20 +0300

    fix: remove placeholder text on JobsListCard.tsx

--- src/app/app/jobs/_components/JobsListCard.tsx
+++ src/app/app/jobs/_components/JobsListCard.tsx
@@ -39,14 +39,6 @@ const JobsListCard = forwardRef<HTMLDivElement, JobsListCardProps>(
               <p className="text-sm text-muted-foreground">{company.name}</p>
             </div>
           </div>
-          <p className="line-clamp-3 text-foreground/70">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-            debitis, delectus ducimus excepturi expedita harum minus nesciunt
-            perferendis quos? A accusantium assumenda atque aut blanditiis culpa
-            debitis doloremque earum eius error excepturi fuga ipsum iure,
-            maxime minima mollitia natus neque obcaecati omnis pariatur
-            perspiciatis, quasi repudiandae tempora vel veritatis voluptatibus?
-          </p>
           <div className="flex flex-wrap items-center gap-2 text-sm font-medium">
             <div className="rounded-md bg-muted p-1 text-muted-foreground">
               {generateReadableEnumLabel(employmentType)}
