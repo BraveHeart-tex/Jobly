2e2f983d8814466d67432c3621d3c2133e5cee3b
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-07-09 14:13:57 +0300

    fix: fix document editor page skeleton styling

--- src/app/home/employee/tools/document-builder/cv-builder/edit/[id]/loading.tsx
+++ src/app/home/employee/tools/document-builder/cv-builder/edit/[id]/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
-    <main className="grid grid-cols-2 fixed top-0 z-50 w-full">
+    <main className="grid xl:grid-cols-2 fixed top-0 z-50 w-full">
       <div className="bg-card min-h-screen p-10 overflow-auto max-h-screen">
         <div className="grid gap-4">
           <Skeleton className="h-12" />
@@ -12,7 +12,7 @@ const Loading = () => {
           <Skeleton className="h-12 w-1/2" />
         </div>
       </div>
-      <div className="bg-muted-foreground dark:bg-secondary min-h-screen">
+      <div className="hiden xl:block bg-muted-foreground dark:bg-secondary min-h-screen">
         <div className="h-12" />
         <div className="h-[90vh] w-[63%] mx-auto pt-4">
           <Skeleton className="h-full w-full bg-secondary dark:bg-muted" />
