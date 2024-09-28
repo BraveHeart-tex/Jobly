c3a9db37925e88a851e078cd1bd1858d05d67b28
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-09-28 09:23:01 +0300

    chore: commit unversioned files

---
+++ src/features/user/profile/repositories/userProfileRepository.ts
@@ -0,0 +1,50 @@
+import type { UserProfileFormSchema } from "@/schemas/user/profile/userProfileFormSchema";
+import { db } from "@/server/db";
+import {
+  educationalBackgrounds,
+  users,
+  workExperiences,
+} from "@/server/db/schema";
+import { desc, eq } from "drizzle-orm";
+
+export const userProfileRepository = {
+  async getUserProfileInformation(
+    userId: number,
+  ): Promise<UserProfileFormSchema | null> {
+    const result = await db.query.users.findFirst({
+      columns: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+      },
+      where: () => eq(users.id, userId),
+      with: {
+        workExperiences: {
+          orderBy: () => desc(workExperiences.startDate),
+        },
+        educationalBackgrounds: {
+          orderBy: () => desc(educationalBackgrounds.startDate),
+        },
+        userSkills: {
+          with: {
+            skill: true,
+          },
+        },
+      },
+    });
+
+    if (!result) return null;
+
+    return {
+      firstName: result.firstName,
+      lastName: result.lastName,
+      educationalBackground: result.educationalBackgrounds,
+      skills: result.userSkills.map((item) => ({
+        ...item.skill,
+        level: item.level,
+      })),
+      workExperiences: result.workExperiences,
+    };
+  },
+};
