3b62b0b25ad345d863f0bcf5d787da5848745446
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-09-30 09:42:47 +0300

    fix: add redirect login on user landing home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,14 @@
+import PageContainer from "@/components/common/PageContainer";
 import { validateRequestByRole } from "@/features/auth/utils";
 
 const AppHomePage = async () => {
   await validateRequestByRole(["candidate", "employer"]);
 
-  return <div>AppHomePage</div>;
+  return (
+    <main>
+      <PageContainer>AppHomePage</PageContainer>
+    </main>
+  );
 };
 
 export default AppHomePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function LandingHome() {
+import { validateRequest } from "@/lib/auth/validateRequest";
+import { SHARED_ROUTES } from "@/lib/routes";
+import { redirect } from "next/navigation";
+
+export default async function LandingHome() {
+  const { user } = await validateRequest();
+
+  if (user) {
+    redirect(SHARED_ROUTES.HOME);
+  }
+
   return <main>Landing home page</main>;
 }
