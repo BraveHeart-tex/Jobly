7d97062ffc077a345136719c115a9466e1bd6788
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-10-04 15:09:58 +0300

    fix: fix typing on document saving schema type

--- src/validators/user/document/saveDocumentDetailsValidator.ts
+++ src/validators/user/document/saveDocumentDetailsValidator.ts
@@ -1,4 +1,4 @@
-import { type InferInput, array, object, optional } from "valibot";
+import { array, object, optional, type InferOutput } from "valibot";
 import { DocumentSectionInsertValidator } from "./documentSectionValidators";
 import { DocumentSectionFieldInsertValidator } from "./sectionFieldValidators";
 import { documentSectionFieldInsertValidator } from "./sectionFieldValueValidators";
@@ -11,6 +11,6 @@ export const SaveDocumentDetailsValidator = object({
   fieldValues: optional(array(documentSectionFieldInsertValidator)),
 });
 
-export type SaveDocumentDetailsData = InferInput<
+export type SaveDocumentDetailsData = InferOutput<
   typeof SaveDocumentDetailsValidator
 >;
